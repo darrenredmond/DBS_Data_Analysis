100e4068d537357b14d1b594c519fcca5a934880
Darren Redmond <c_dredmond@groupon.com>
2017-01-28 11:49:08 +0000

    Central Tendency

--- Central_Tendency/customTests.R
+++ Central_Tendency/customTests.R
@@ -1,3 +1,21 @@
+# Get the swirl state
+getState <- function(){
+  # Whenever swirl is running, its callback is at the top of its call stack.
+  # Swirl's state, named e, is stored in the environment of the callback.
+  environment(sys.function(1))$e
+}
+
+# Get the value which a user either entered directly or was computed
+# by the command he or she entered.
+getVal <- function(){
+  getState()$val
+}
+
+# Get the last expression which the user entered at the R console.
+getExpr <- function(){
+  getState()$expr
+}
+
 runTest <- function(...)UseMethod("runTest")
 
 runTest.exact <- function(keyphrase,e){
@@ -18,24 +36,6 @@ runTest.equivalent <- function(keyphrase,e) {
   return(omnitest(rightside(keyphrase)))
 }
 
-# Get the swirl state
-getState <- function(){
-  # Whenever swirl is running, its callback is at the top of its call stack.
-  # Swirl's state, named e, is stored in the environment of the callback.
-  environment(sys.function(1))$e
-}
-
-# Get the value which a user either entered directly or was computed
-# by the command he or she entered.
-getVal <- function(){
-  getState()$val
-}
-
-# Get the last expression which the user entered at the R console.
-getExpr <- function(){
-  getState()$expr
-}
-
 loadDigest <- function(){
   if (!require("digest")) install.packages("digest")
   library(digest)
@@ -49,13 +49,6 @@ dbs_on_demand <- function(){
     email <- readline("What is your email address? ")
     student_number <- readline("What is your student number? ")
     hash <- digest(paste(course, student_number), "md5", serialize = FALSE)
-
-    payload <- sprintf('{
-      "course": "%s",
-      "email": "%s",
-      "student_number": "%s",
-      "hash": "%s",
-    }', course, email, student_number, hash)
     url <- paste('http:///results.dbsdataprojects.com/course_results/submit?course=', course, '&hash=', hash, '&email=', email, '&student_number=', student_number, sep='')
 
     respone <- httr::GET(url)
